e8542fe4f581cf185edff7fc5325abe0cce35bfe
Teddy Medjio <nganmedjiojeanclaude@gmail.com>
2024-01-07 14:00:26 +0100

    about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,54 +11,6 @@ const page = () => {
         <h1 className="text-blu-50 extra-desk font-header">
           where great ideas
         </h1>
-        <h1 className="text-blu-50 extra-desk font-header">
-          where great ideas
-        </h1>
-        <h1 className="text-blu-50 extra-desk font-header">
-          where great ideas
-        </h1>
-        <h1 className="text-blu-50 extra-desk font-header">
-          where great ideas
-        </h1>
-        <h1 className="text-blu-50 extra-desk font-header">
-          where great ideas
-        </h1>
-        <h1 className="text-blu-50 extra-desk font-header">
-          where great ideas
-        </h1>
-        <h1 className="text-blu-50 extra-desk font-header">
-          where great ideas
-        </h1>
-        <h1 className="text-blu-50 extra-desk font-header">
-          where great ideas
-        </h1>
-        <h1 className="text-blu-50 extra-desk font-header">
-          where great ideas
-        </h1>
-        <h1 className="text-blu-50 extra-desk font-header">
-          where great ideas
-        </h1>
-        <h1 className="text-blu-50 extra-desk font-header">
-          where great ideas
-        </h1>
-        <h1 className="text-blu-50 extra-desk font-header">
-          where great ideas
-        </h1>
-        <h1 className="text-blu-50 extra-desk font-header">
-          where great ideas
-        </h1>
-        <h1 className="text-blu-50 extra-desk font-header">
-          where great ideas
-        </h1>
-        <h1 className="text-blu-50 extra-tablet font-header">
-          meet great designs
-        </h1>
-        <h1 className="text-blu-50 extra-tablet font-header">
-          meet great designs
-        </h1>
-        <h1 className="text-blu-50 extra-tablet font-header">
-          meet great designs
-        </h1>
       </div>
     </>
   );
